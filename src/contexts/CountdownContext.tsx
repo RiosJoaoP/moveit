0700f936e8936afaafe21f2fbc5f476f6bdae7c5
John Rios <joao.paulo.rios@hotmail.com>
2021-02-28 17:30:44 -0300

    feat: ajustando contador para deploy

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -37,7 +37,7 @@ export function CountdownProvider({ children }: CountdownProviderProps){
         clearTimeout(countdownTimeout);
         setIsActive(false);
         setHasFinished(false);
-        setTime(0.05 * 60);
+        setTime(25 * 60);
     }
     
     useEffect(() => {
